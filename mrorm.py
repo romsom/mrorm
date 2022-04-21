42d79542ac39a58d4a9a894da3027d558d7baeee
Roman Sommer <roman@resonant-bytes.de>
2022-04-21 12:33:03 +0200

    mrorm: implement basic dynamic table subtyping

--- mrorm.py
+++ mrorm.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+from typing import Dict, List, Type
 import sqlite3
 import sys
 from db_base import DB_Element, DB_Element_With_Foreign_Key
+
+db_classes: Dict[str, Type] = {}
 
 def get_tables(con):
     cursor = con.cursor()
@@ -18,11 +21,38 @@ def get_cols_for_table(con, table: str):
     cursor.close()
     return res
 
+def get_db_sub_class(name: str, cols: List[str], pk: List[str]) -> Type:
+    # TODO: constraints (primary, foreign)
+    class _DB_Element(DB_Element):
+        primary_key = pk
+        _table_name = name
+        _cols = cols
+        def __init__(self, **kwargs):
+            # set all supplied attributes
+            for col in self._cols:
+                if col in kwargs.keys():
+                    self.__dict__[col] = kwargs[col]
+                else:
+                    self.__dict__[col] = None
+                remaining_keys = list(set(kwargs.keys()) - set(self._cols))
+                if len(remaining_keys) > 0:
+                    print(f'{self._table_name}: superfluous parameters in constructor: {remaining_keys}', file=sys.stderr)
+
+    return _DB_Element
+
 
 if __name__ == '__main__':
     con = sqlite3.connect(sys.argv[1])
     tables = get_tables(con)
     for table in tables:
         print(table)
-        print(get_cols_for_table(con, table))
+        cols = get_cols_for_table(con, table)
+        print(cols)
+        # TODO: primary key
+        db_classes[table] = get_db_sub_class(table, cols, [])
+
+    new_data = db_classes['mydata'](name="hello", id=42)
+    new_data.insert(con)
+    null_data = db_classes['mydata']()
+    print(null_data.lookup(con))
     con.close()
